e4857b74bed27c50e260cc477c7a99a883f09684
Christian Fufezan <christian@fufezan.net>
2020-11-20 14:55:40 +0000

    Post Discussion 20.11

--- tests/unittests/uio/test_uio_calculate_md5.py
+++ tests/unittests/uio/test_uio_calculate_md5.py
@@ -2,8 +2,19 @@ import os
 
 
 
+                                                             88
+                                                             88
+                                                             88
+88       88  8b,dPPYba,  ,adPPYba,   ,adPPYb,d8  ,adPPYYba,  88
+88       88  88P'   "Y8  I8[    ""  a8"    `Y88  ""     `Y8  88
+88       88  88           `"Y8ba,   8b       88  ,adPPPPP88  88
+"8a,   ,a88  88          aa    ]8I  "8a,   ,d88  88,    ,88  88
+ `"YbbdP'Y8  88          `"YbbdP"'   `"YbbdP"Y8  `"8bbdP"Y8  88
+                                     aa,    ,88
+                                      "Y8bbdP"
 """
 
 
 
     os.remove(output.name)
+    assert output_md5 == "8d6c9484a6b6f329a9ae57a1f8837e7e"
