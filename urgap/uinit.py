e281c52c2c3c92a233af38e3b506fd470d70205a
Christian Fufezan <christian@fufezan.net>
2021-02-24 11:10:51 +0000

    Add resource copy during init

--- urgap/uinit.py
+++ urgap/uinit.py
@@ -1,5 +1,15 @@
 import json
 
+    target_resources_path.mkdir(exist_ok=True)
+    for rfile in source_resources_path.glob("**/*"):
+        if rfile.is_file() is True:
+            target_rfile = Path(
+                str(rfile).replace(
+            )
+                target_rfile.parent.mkdir(exist_ok=True, parents=True)
+                shutil.copy(rfile, target_rfile)
+
+
 
         target_json_path = Path(
         )
