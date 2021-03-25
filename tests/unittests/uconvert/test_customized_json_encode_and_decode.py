c5154fe362bb74e72f88a14801af686fb81d7899
Christian Fufezan <christian@fufezan.net>
2021-03-25 09:44:32 +0000

    Apply yesterdays changes and ideas

---
+++ tests/unittests/uconvert/test_customized_json_encode_and_decode.py
@@ -0,0 +1,15 @@
+import datetime
+import json
+
+
+
+def test_date_formats_are_converted_properly():
+    L_date_obj = datetime.datetime.fromisoformat("2008-05-25T02:11:00.000000")
+
+    history_entry = {
+        "uname": "L!",
+        "created": L_date_obj,
+    }
+    assert "2008-05-25T02:11:00" in h
+    assert history_entry_recreated["created"] == L_date_obj
+
---
+++ urgap/uconvert/json.py
@@ -0,0 +1,15 @@
+import datetime
+# inspired by https://gist.github.com/simonw/7000493
+
+
+class JSONEncoder(json.JSONEncoder):
+
+        if isinstance(obj, datetime.datetime):
+
+class JSONDecoder(json.JSONDecoder):
+            *args,
+            object_hook=self.object_hook,
+            **kwargs,
+        )
+
+                return datetime.datetime.fromisoformat(obj["value"])
