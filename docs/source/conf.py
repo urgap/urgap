242f75c27b402a134ca1f4348ec71a8b698147c2
ArtiVlasov <67375717+ArtiVlasov@users.noreply.github.com>
2021-05-14 13:50:20 +0000

    * version is now imported from the version.txt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,11 +9,14 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+import os
 
 # -- Project information -----------------------------------------------------
 
 
+# The full version, including alpha/beta/rc tags stored in version.txt
 
+# The full version, including alpha/beta/rc tags.
 
 # -- General configuration ---------------------------------------------------
 
