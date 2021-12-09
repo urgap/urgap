3adeb9caa71973da8cb4b5902d894ab1f60245a2
Christian Fufezan <christian@fufezan.net>
2021-12-09 08:34:46 +0000

    Add fixture for clean test dirs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import pytest
 
 
+
+
 def tmp_dir():
--- tests/integrationtests/rerun_is_skipped/test_regular.py
+++ tests/integrationtests/rerun_is_skipped/test_regular.py
@@ -4,11 +4,14 @@ import pytest
 
 
 @pytest.mark.parametrize(
+    "provide_clean_test_node_dirs",
     [
         (
             ),
     ],
+    indirect=["provide_clean_test_node_dirs"],
 )
+def test_node_workflow_rerun_is_skipped_simple(provide_clean_test_node_dirs):
     print("Input:")
     print("Output node1:")
     pprint.pprint(return_file)
