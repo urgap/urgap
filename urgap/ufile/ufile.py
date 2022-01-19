3c4ecd1a5dd1a9b24517b0d05863dc90b655d323
Christian Fufezan <christian@fufezan.net>
2022-01-19 14:02:04 +0000

    Add example script

--- urgap/ufile/ufile.py
+++ urgap/ufile/ufile.py
@@ -6,10 +6,19 @@ class UFile:
     def __init__(
         self,
         self._local_copy = None
+        self._io = None
+
+
+
+
+    @property
+        if self._io is None:
+        return self._io
 
     @classmethod
 
 
 
         )
+        self._io = None
 
