136f1513ac902d4a8c08390119c63a8f35796340
Christian Fufezan <christian@fufezan.net>
2022-01-25 12:04:58 +0000

    Extend test to cover aux files

--- tests/unittests/uconvert/test_customized_json_encode_and_decode.py
+++ tests/unittests/uconvert/test_customized_json_encode_and_decode.py
@@ -13,3 +13,9 @@ def test_date_formats_are_converted_properly():
     assert "2008-05-25T02:11:00" in h
     assert history_entry_recreated["created"] == L_date_obj
 
+
+def test_list_of_UFiles_objects_are_converted_properly():
+    payload = {"ufiles": [ufile_1, ufile_2]}
+    assert ufile_1.as_uri() in h
+    assert ufile_2.as_uri() in h
+    assert payload["ufiles"] == [ufile_1, ufile_2]
