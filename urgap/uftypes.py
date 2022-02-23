43210ec62ce5e21e0fa6d6af7fa1245f76388259
Tristan Ranff <git@ranff.me>
2022-02-23 12:19:17 +0000

    namespace now build tree (not other way around(

---
+++ urgap/uftypes.py
@@ -0,0 +1,33 @@
+import types
+
+proteomics = types.SimpleNamespace()
+proteomics.ANY = "proteomics.ANY"
+proteomics.dbsearch = types.SimpleNamespace()
+proteomics.dbsearch.ANY = "proteomics.dbsearch.ANY"
+proteomics.dbsearch.COMET_MZID = ".comet.mzid"
+proteomics.dbsearch.MASCOT_DAT = ".mascot.dat"
+proteomics.dbsearch.MSAMANDA_CSV = ".msamanda.csv"
+proteomics.dbsearch.MSGFPLUS_MZID = ".msgfplus.mzid"
+proteomics.dbsearch.OMSSA_CSV = ".omssa.csv"
+proteomics.dbsearch.XTANDEM_XML = ".xtandem.xml"
+
+# Data Conversion
+proteomics.converter = types.SimpleNamespace()
+proteomics.converter.ANY = "proteomics.converter.ANY"
+proteomics.converter.PYMZML_MGF = ".pymzml.mgf"
+
+# Validator
+proteomics.validator = types.SimpleNamespace()
+proteomics.validator.ANY = "proteomics.validator.ANY"
+proteomics.validator.PERCOLATOR_CSV = ".percolator.csv"
+proteomics.validator.PEPTIDEFOREST_CSV = ".peptideforest.csv"
+
+# Quantification
+proteomics.quantification = types.SimpleNamespace()
+proteomics.quantification.ANY = "proteomics.quantification.ANY"
+
+stats = types.SimpleNamespace()
+
+plotter = types.SimpleNamespace()
+plotter.ANY = "plotter.ANY"
+plotter.VOLCANO_PDF = ".volcano.pdf"
