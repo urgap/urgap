ac01c96d4921d65a9761acbb104a0e8c345564aa
Tristan Ranff <git@ranff.me>
2022-02-28 13:27:16 +0000

    no more fixture

--- tests/unittests/ufile_list/test_filter_ufile_list.py
+++ tests/unittests/ufile_list/test_filter_ufile_list.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 
 
+def test_filtering_on_single_file_(tmp_scratch_disk):
 
     )
 
@@ -10,6 +11,7 @@ from pathlib import Path
     assert filtered_list == [uf]
 
 
+def test_filtering_on_two_files_one_excluding(tmp_scratch_disk):
 
     )
     )
@@ -21,6 +23,7 @@ from pathlib import Path
 
 
 def test_filtering_on_two_files_one_excluding_based_on_additional_filters(
+    tmp_scratch_disk,
 ):
 
     )
@@ -33,6 +36,7 @@ def test_filtering_on_two_files_one_excluding_based_on_additional_filters(
     assert filtered_list == [uf_2]
 
 
+def test_filtering_on_single_file_with_minium_zero(tmp_scratch_disk):
 
     )
 
@@ -42,6 +46,7 @@ def test_filtering_on_two_files_one_excluding_based_on_additional_filters(
     assert filtered_list == []
 
 
+def test_filtering_on_single_file_with_minium_zero_one_incl(tmp_scratch_disk):
 
     )
 
@@ -51,6 +56,7 @@ def test_filtering_on_two_files_one_excluding_based_on_additional_filters(
     assert filtered_list == [uf_1]
 
 
+def test_wrapper_definitions_are_met(tmp_scratch_disk):
     ufiles = [
         ),
         ),
@@ -63,6 +69,7 @@ def test_filtering_on_two_files_one_excluding_based_on_additional_filters(
     assert len(filtered_list) == 3
 
 
+def test_wrapper_definitions_are_met_max(tmp_scratch_disk):
     ufiles = [
         ),
         ),
@@ -77,6 +84,7 @@ def test_filtering_on_two_files_one_excluding_based_on_additional_filters(
     assert len(filtered_list) == 5
 
 
+def test_wrapper_defintions_are_met_with_additional_filters(tmp_scratch_disk):
     ufiles = [
         ),
         ),
@@ -92,6 +100,7 @@ def test_filtering_on_two_files_one_excluding_based_on_additional_filters(
 
 
 def test_wrapper_defintions_are_met_with_additional_filters_nested_structure(
+    tmp_scratch_disk,
 ):
     ufiles = [
         ),
@@ -111,6 +120,7 @@ def test_wrapper_defintions_are_met_with_additional_filters_nested_structure(
     assert len(filtered_list) == 3
 
 
+def test_wrapper_defintions_are_not_met_with_additional_filters(tmp_scratch_disk):
     ufiles = [
         ),
         ),
