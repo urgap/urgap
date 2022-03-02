f4e009a1e71cbb8933926734920cf5c8069cfe0a
ArtiVlasov <67375717+ArtiVlasov@users.noreply.github.com>
2022-03-02 19:49:35 +0000

    * added delete_tmp_files tests

--- tests/unittests/unode/test_delete_tmp_files.py
+++ tests/unittests/unode/test_delete_tmp_files.py
@@ -1,3 +1,23 @@
+import os
+
+def test_unode_delete_tmp_file():
+        fin.write("This is gonna be deleted")
+    assert tmp_file.exists() is True
+    test_node.tmp_files.append(str(tmp_file))
+    test_node.delete_tmp_files()
+
+    assert tmp_file.exists() is False
+
+
+def test_unode_delete_tmp_dir():
+    tmp_dir.mkdir(parents=True, exist_ok=True)
+    os.symlink(tmp_dir, tmp_dir2)
+    assert tmp_dir.exists() is True
+    assert tmp_dir2.exists() is True
+    test_node.tmp_files.extend([tmp_dir2, tmp_dir])
+    test_node.delete_tmp_files()
+    assert tmp_dir.exists() is False
+    assert tmp_dir2.exists() is False
 
 
 @pytest.mark.parametrize(
