17df490f52df5c34993e5341553244d2dd2fd17a
ArtiVlasov <67375717+ArtiVlasov@users.noreply.github.com>
2022-03-03 11:29:45 +0000

    * added first test

--- tests/unittests/uinit/test_uinit.py
+++ tests/unittests/uinit/test_uinit.py
@@ -10,6 +10,13 @@ def test_create_home_folder(tmp_dir):
     )
 
 
+
+        f.write("New Content")
+
+    assert md5 != md5_new_content
+    assert md5_new_content == md5_second_push
+
+
 def test_copy_config_if_needed(tmp_dir):
 
 
