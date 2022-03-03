4ee9806f2e40323677d35d8c786d403a84fbfcde
Tristan Ranff <git@ranff.me>
2022-03-03 10:04:17 +0000

    add tests for utree and ftypes

---
+++ tests/unittests/utree/test_uftypes.py
@@ -0,0 +1,12 @@
+import regex as re
+
+
+def test_node_names_are_unique():
+    assert len(nodes) == len(set(nodes))
+
+
+def test_leaf_suffix_nomenclature():
+    leafs = leafs.difference(test_leafs).difference({".unknown"})
+
+    suffix_pattern = re.compile(r"^\.[\w]+\.[\w]+$")
+    assert all(bool(re.match(suffix_pattern, s)) for s in leafs)
