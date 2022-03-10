b76276b4eaf1acac14cdbbbcfed3a4ba85365480
Christian Fufezan <christian@fufezan.net>
2022-03-10 13:39:36 +0000

    Add uuid to scratch path

--- tests/unittests/ufile/file/test_folder_structure_is_set.py
+++ tests/unittests/ufile/file/test_folder_structure_is_set.py
@@ -5,3 +5,5 @@ from pathlib import Path
 def test_folder_is_set_properly_on_scratch_disk():
     content = Path("test_node_data/test.txt")
     set_path = uf.io.scratch_path
+    for tp in theoretical_path.parts:
+        assert tp in set_path.parts
