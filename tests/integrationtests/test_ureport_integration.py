7483f6481ac61b96defbb65b0cb5f51d6a09cce3
ArtiVlasov <67375717+ArtiVlasov@users.noreply.github.com>
2022-03-14 16:46:46 +0000

    * ureport integration test to fully test parent functionality

---
+++ tests/integrationtests/test_ureport_integration.py
@@ -0,0 +1,18 @@
+import networkx
+
+
+    )
+
+
+    filtered_1 = filter_node.run(
+        ufiles=[input_file],
+    )
+    filtered_1b = filter_node.run(
+        ufiles=[input_file],
+    )
+    filtered_1c = filter_node.run(
+        ufiles=filtered_1b,
+    )
+    filtered_2 = filter_node.run(
+        ufiles=[filtered_1, filtered_1c, input_file],
+    )
