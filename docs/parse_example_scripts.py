8f14624ac098bc313677502d6e2d979fec3b0a2a
ArtiVlasov <67375717+ArtiVlasov@users.noreply.github.com>
2022-03-16 15:43:06 +0000

    * added docu generation for example scripts

---
+++ docs/parse_example_scripts.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+
+
+"""
+
+
+
+def main():
+            print(""".. code-block:: python\n""", file=o)
+                for line in infile:
+                    print("\t{0}".format(line.rstrip()), file=o)
+
+
+if __name__ == "__main__":
+    main()
