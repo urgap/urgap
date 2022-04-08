b7f399c8e02000587d466359dbdf09e29c71b8fb
Christian Fufezan <christian@fufezan.net>
2022-04-08 09:00:07 +0000

    Template for alt utree

--- urgap/uftypes.py
+++ urgap/uftypes.py
@@ -55,8 +55,12 @@ plotter.ANY = "plotter.ANY"
 plotter.VOLCANO_PDF = ".volcano.pdf"
 plotter.UMAP_PDF = ".umap.pdf"
 plotter.VENN_RESULTS_CSV = ".venn_results.csv"
+plotter.VENN_RESULTS_SVG = ".venn_results.svg"
 plotter.UMAP_PDF = ".umap.pdf"
 # Filetype =======================================================================
+any = types.SimpleNamespace()
+any.CSV = ".any.csv"
+any.SVG = ".any.svg"
 
 # Metabolomics==================================================================
 # Imaging=======================================================================
