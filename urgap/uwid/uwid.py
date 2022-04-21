474993fe69b2283d1e4488241c10dca6d2611cdc
Tristan Ranff <git@ranff.me>
2022-04-21 07:02:03 +0000

    move UWIDGenerator class out of init

---
+++ urgap/uwid/uwid.py
@@ -0,0 +1,30 @@
+
+# https://github.com/moby/moby/blob/master/pkg/namesgenerator/names-generator.go
+# ^--- too look at ...
+
+
+class UWIDGenerator:
+
+
+
+    All issued WIDs are captured in self.issued_wids and those
+    """
+
+        self.nouns = self._read_word_file("nouns.txt")
+        self.verbs = self._read_word_file("verbs_3rd_form.txt")
+        self.adjectives = self._read_word_file("adjectives.txt")
+        self.adverbs = self._read_word_file("adverbs.txt")
+        self.prepositions = self._read_word_file("prepositions.txt")
+        self.indefinitpronomen = self._read_word_file("indefinitpronomen.txt")
+        self.issued_wids = []
+
+        word_file = Path(__file__).parent / f"{word_type}"
+
+    @property
+
+        return self
+
+        return self.generate_wid()
+
+        self.issued_wids.append(new_wid)
+        return new_wid
