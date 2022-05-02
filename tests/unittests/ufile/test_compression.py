f67f6c629aecbbe306b79bb4b14b8fcd80eb4055
Tristan Ranff <git@ranff.me>
2022-05-02 12:32:10 +0000

    uncompress from tar to ufilelist

--- tests/unittests/ufile/test_compression.py
+++ tests/unittests/ufile/test_compression.py
@@ -9,6 +9,8 @@ def test_uncompress_zip(tmp_scratch_disk):
 def test_uncompress_tar_gz(tmp_scratch_disk):
     content = Path("compressions/test.txt.tar.gz")
     )
+    new_ufl = uf.uncompress()
+    assert new_ufl[0].path.read_text() == "twas_uncompressed"
 
 
 def test_compress_zip(tmp_scratch_disk):
@@ -22,4 +24,5 @@ def test_compress_tar_gz(tmp_scratch_disk):
     content = Path("compressions/test.txt")
     )
     assert new_uf.path.exists() is True
+    assert new_uf.uncompress()[0].path.read_text() == "twas_uncompressed"
     assert uf.as_uri() + ".tar.gz" == new_uf.as_uri()
