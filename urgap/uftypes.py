25208ba2004bd4c9b6f9970e4c0bb126442adfa4
ArtiVlasov <67375717+ArtiVlasov@users.noreply.github.com>
2022-05-06 09:28:26 +0000

    include avg_stitch node

--- urgap/uftypes.py
+++ urgap/uftypes.py
@@ -84,6 +84,7 @@ ms.SPECTRA_META_CSV = ".spectra_meta.csv"
 ms.SCANS_CSV = ".scans.csv"
 ms.NORM_IT_CSV = ".norm_it.csv"
 ms.ALIGN_SCANS_CSV = ".align_scans.csv"
+ms.AVG_SCANS_CSV = ".avg_scans.csv"
 
 ms.converter = types.SimpleNamespace()
 ms.converter.ANY = "ms.converter.ANY"
