588c45b58e71bc9fddec7cb879128e644058fe2a
ArtiVlasov <67375717+ArtiVlasov@users.noreply.github.com>
2022-05-31 16:02:24 +0000

    * added avg_files node (binpeaks)

--- urgap/uftypes.py
+++ urgap/uftypes.py
@@ -90,6 +90,7 @@ ms.ALIGN_SCANS_CSV = ".align_scans.csv"
 ms.AVG_SCANS_CSV = ".avg_scans.csv"
 ms.RECAL_MZ_CSV = ".recal_mz.csv"
 ms.MULTI_ALIGN_SCANS_CSV = ".multialign_scans.csv"
+ms.MULTI_AVG_SCANS_CSV = ".multiavg_scans.csv"
 
 ms.converter = types.SimpleNamespace()
 ms.converter.ANY = "ms.converter.ANY"
