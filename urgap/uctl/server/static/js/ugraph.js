c59ce32175e1696caa66a81a44c121552447c5e6
Christian Fufezan <christian@fufezan.net>
2022-07-18 07:39:24 +0000

    Add zoom, mouse over and coloring to execution graph

--- urgap/uctl/server/static/js/ugraph.js
+++ urgap/uctl/server/static/js/ugraph.js
@@ -6,5 +6,38 @@
 
 
 
+
+
+
+        .force('charge', d3.forceManyBody().strength(-70))
+        .force('collision', d3.forceCollide().radius(radius * 2))
+        .on("tick", tickActions);
+
+
+        .attr("class", "links")
+        .selectAll("line")
+        .data(network.links)
+        .enter().append("line")
+        .attr("stroke-width", 4)
+
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(network.nodes)
+        .attr("r", radius)
+
+
+
+
+
+
+
     }
 
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+
+
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
