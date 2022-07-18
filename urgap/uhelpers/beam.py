2c5ecc65bb3b612ca96185e90569e39bb868dc27
Christian Fufezan <christian@fufezan.net>
2022-07-18 09:36:07 +0000

    Add beam implementation examples and uhelper class

---
+++ urgap/uhelpers/beam.py
@@ -0,0 +1,55 @@
+
+
+
+
+        Args:
+        """
+        self.ready = False
+        self.window = beam.transforms.window.GlobalWindow()
+        self.unode = None
+        if self._check_input(unode=unode, urd=urd):
+            self.ready = True
+
+
+        Args:
+
+        Returns:
+        """
+        input_is_ok = True
+            input_is_ok = False
+            input_is_ok = False
+            )
+        return input_is_ok
+
+
+
+
+        Args:
+
+        Yields:
+        """
+        if self.ready is True:
+
+
+
+
+
+
+
+    Args:
+
+    Returns:
+    """
+    from apache_beam.runners.interactive.display import pipeline_graph
+
+    s = pipeline_graph.PipelineGraph(pipeline).get_dot()
+    net = Network(
+        width="100%",
+        directed=True,
+        font_color="white",
+    )
+    net.use_DOT = True
+    net.dot_lang = " ".join(s.split("\n"))
+    net.dot_lang = net.dot_lang.replace('"', '\\"')
+    net.dot_lang = net.dot_lang.replace("fontcolor=blue", "fontcolor=white")
+    return net
