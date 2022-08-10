de3ebdaada9750910ed53a7e63dc1868d1a5078e
Tristan Ranff <git@ranff.me>
2022-08-10 18:53:35 +0000

    added test/fix for uncompress on multiple files

--- tests/unittests/ufile/test_compression.py
+++ tests/unittests/ufile/test_compression.py
@@ -30,3 +30,13 @@ def test_compress_tar_gz(tmp_scratch_disk):
     assert new_uf.path.exists() is True
     assert new_uf.uncompress()[0].path.read_text() == "twas_uncompressed"
     assert uf.as_uri() + ".tar.gz" == new_uf.as_uri()
+
+
+def test_multiple_files(tmp_scratch_disk):
+    content = Path("compressions/asdf.zip")
+    )
+    new_ufl = uf.uncompress()
+    # Directory and two test files
+    assert len(new_ufl) == 2
+    assert new_ufl[0].path.read_text() == f"test_{new_ufl[0].simple_name}"
+    assert new_ufl[1].path.read_text() == f"test_{new_ufl[1].simple_name}"
