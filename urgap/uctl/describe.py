2ca5eea316c0bd403793744bbad423846d9c06ce
Christian Fufezan <christian@fufezan.net>
2022-10-04 10:12:42 +0000

    Update uctl to have click wrappers

--- urgap/uctl/describe.py
+++ urgap/uctl/describe.py
@@ -4,7 +4,14 @@ import click
 
 @click.command()
 @click.argument("wid")
+    """Retrieve UMeta information for a given WID (click wrapper).
+
+    """
+
+
 
 
 @click.command()
 @click.argument("object_name")
+
+
