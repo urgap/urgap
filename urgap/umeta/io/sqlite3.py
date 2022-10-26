8deaaaac72f26d4e223977fade35615a1eb27485
Christian Fufezan <christian@fufezan.net>
2022-10-26 16:35:27 +0000

    Add sqlite using sqlachemy for simpla umeta storage

---
+++ urgap/umeta/io/sqlite3.py
@@ -0,0 +1,11 @@
+"""UMeta subclass for using the sqlite interface."""
+
+
+
+    """UMeta sqlite class.
+
+    SQlite is not intended for production use!
+    """
+
+        self._db = None
+        self._session = None
