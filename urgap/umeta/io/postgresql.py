d8e8c66680d305e1ddaf971ac45f01dc6fa3d0d0
Christian Fufezan <christian@fufezan.net>
2022-11-15 10:30:16 +0000

    Rename postgres

---
+++ urgap/umeta/io/postgresql.py
@@ -0,0 +1,12 @@
+"""UMeta subclass for using the sqlite interface."""
+
+
+
+    """UMeta postgresql class."""
+
+        self._db = None
+        self._session = None
+
+        )
+        connection_string = postgresql_uri.replace(
+        )
