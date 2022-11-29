5d11146ee7cb3a4877d5f54a42feb893d9788b4f
Christian Fufezan <christian@fufezan.net>
2022-11-29 15:23:13 +0000

    Add uuri_dict class

--- urgap/ucredentials/ucredentials.py
+++ urgap/ucredentials/ucredentials.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 
 
+class UCredentialManager:
     """UCredentials Manager class.
 
     The credential Manager extracts the secrets from the secret store.
@@ -16,19 +17,45 @@ from pathlib import Path
         """
 
         self.ID_KEY = credentials_id_key
+        self._extracted_secrets = {}
 
+        for cred_entry in self.read_credentials(json_path=json_path):
+            self.ingest_cred_entry(cred_entry)
 
 
         Args:
 
         Returns:
         """
+            cred_key = self.format_cred_key(ce_or_ck)
+            user = self.extract_credentials(cred_key, force=force)["user"]
+        elif isinstance(ce_or_ck, str):
+            user = self.extract_credentials(ce_or_ck, force=force)["user"]
         else:
+            user = None
+        return user
 
 
         Args:
 
+        Returns:
         """
+            cred_key = self.format_cred_key(ce_or_ck)
+            password = self.extract_credentials(cred_key, force=force)["password"]
+        elif isinstance(ce_or_ck, str):
+            password = self.extract_credentials(ce_or_ck, force=force)["password"]
+        else:
+            password = None
+        return password
+
+        return self._extracted_secrets[cred_key]
+
+
+        Args:
+        """
+        cred_key = self.format_cred_key(cred_entry)
+        cred_entry = self.validate_credential_entry(cred_entry)
+            del self._extracted_secrets[cred_key]
 
 
         Args:
@@ -41,6 +68,7 @@ from pathlib import Path
         Args:
         """
         for cred_entry in credential_list:
+            self.ingest_cred_entry(cred_entry)
 
 
         Args:
@@ -51,13 +79,29 @@ from pathlib import Path
             c_key = self.ID_KEY.format(**cred_entry)
         return c_key
 
+        """Read from credentials_lookup.json.
 
         Args:
 
         Returns:
         """
+        if json_path is None:
+        cred_json = {}
+                cred_json = json.load(uj)
+        else:
+        return cred_json["credentials"]
 
+    def write_credentials(
+        self,
 
         Args:
 
         """
+        if json_path is None:
+            json.dump(
+                {
+                    "description": description,
+                },
+                uj,
+                indent=4,
+            )
