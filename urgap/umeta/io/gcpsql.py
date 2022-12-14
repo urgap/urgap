494934b40a0ac413a4459bd03baafff2378d3071
Christian Fufezan <christian@fufezan.net>
2022-12-14 11:05:56 +0000

    Add Umeta GCP Cloud SQL and default placeholder config entries

---
+++ urgap/umeta/io/gcpsql.py
@@ -0,0 +1,15 @@
+"""UMeta subclass for using the sqlite interface."""
+
+
+    """UMeta GCP Cloud SQL class."""
+
+        self._db = None
+        self._session = None
+
+
+        Returns:
+        """
+        )
+        )
+        )
+        return connection_string.format(**credentials)
