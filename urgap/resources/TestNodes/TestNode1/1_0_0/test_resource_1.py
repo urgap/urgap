0699adad9806d000ee0fcb3d72c54ef4575750d7
ArtiVlasov <67375717+ArtiVlasov@users.noreply.github.com>
2022-12-20 23:10:58 +0000

    * fixed test_node_v1, so the unode.import_engine_as_python_function can still be used

--- urgap/resources/TestNodes/TestNode1/1_0_0/test_resource_1.py
+++ urgap/resources/TestNodes/TestNode1/1_0_0/test_resource_1.py
@@ -1,10 +1,20 @@
 import argparse
 
 
+    if output_files is not None:
+        for output_file in output_files:
+                print(params, file=oo)
+                print(input_files, file=oo)
+    return "I am a yummy test dummy!"
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", dest="input_files", nargs="+")
     parser.add_argument("--output", dest="output_files", nargs="+")
     parser.add_argument(
         "--params",
         dest="params",
+    )
+    main(
     )
