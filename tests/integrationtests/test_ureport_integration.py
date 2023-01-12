9e4c7f362c9da054683c1eb1a01857854ef66e23
Christian Fufezan <christian@fufezan.net>
2023-01-12 15:13:36 +0000

    Fix test_ureport_integration

--- tests/integrationtests/test_ureport_integration.py
+++ tests/integrationtests/test_ureport_integration.py
@@ -4,6 +4,7 @@ import networkx
 
     )
 
+    assert isinstance(ur.graph, networkx.classes.digraph.DiGraph) is True
 
     filtered_1 = filter_node.run(
         ufiles=[input_file],
@@ -17,3 +18,5 @@ import networkx
     filtered_2 = filter_node.run(
         ufiles=[filtered_1, filtered_1c, input_file],
     )
+
+    assert len(ur.graph.nodes) == 9
