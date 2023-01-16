f4b66b0af36b5f09a267f4f0095aa0e44b21d4bb
Christian Fufezan <christian@fufezan.net>
2023-01-16 08:09:38 +0000

    Replaced utrace.dooh with utrace.urun_dict.dooh

--- urgap/unodes/test_nodes/test_node_5.py
+++ urgap/unodes/test_nodes/test_node_5.py
@@ -40,10 +40,14 @@
             difference = n - minimal_dataset.get(filetype, 0)
                 utrace.extend_output_files_by_uftype(uftype=filetype)
 
+        utrace.urun_dict.command_list = [
             "python",
             str(self.exe_path),
             "--params",
             "--input",
         ]
         for ufile in utrace.input_files:
+            utrace.urun_dict.command_list.append(str(ufile.path))
+        utrace.urun_dict.command_list.append("--output")
         for ufile in utrace.output_files:
+            utrace.urun_dict.command_list.append(str(ufile.path))
