830d4061689de5eb206dfa803a7663d4226a391f
Christian Fufezan <christian@fufezan.net>
2023-03-09 10:44:20 +0000

    Add simple test to check we do not modify outside urun_dict (as before)

---
+++ tests/unittests/unode/test_doesnt_modify_run_dict.py
@@ -0,0 +1,20 @@
+import tempfile
+
+
+def test_out_side_urun_dict_is_not_modified_by_kwargs():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+            {
+                "parameters": {
+                    "triggers_nuttin": 100,
+                    "triggers_rerun": 100,
+                    "triggers_rerun_-3": 100,
+                },
+                "unode_parameters": {
+                    "record_skipped_runs": True,
+                    "storage_base_uri": f"file://{tmpdirname}",
+                },
+        )
+            [
+        )
+        _ = test_node1.run(ufiles=ufiles, urun_dict=urun_dict, force=True)
+        assert urun_dict.unode_parameters.get("force", None) is not True
