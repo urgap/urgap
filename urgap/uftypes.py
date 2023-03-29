4616a5599b3dfc3417f47e186ac9b33b141833b8
ArtiVlasov <67375717+ArtiVlasov@users.noreply.github.com>
2023-03-29 09:46:45 +0000

    * added test files and enhanced the test suite

--- urgap/uftypes.py
+++ urgap/uftypes.py
@@ -179,12 +179,14 @@ exp_design.input.ANY = "exp_design.input.ANY"
 exp_design.input.UTMX_METADATA_XLSX = ".utmx_metadata.xlsx"
 exp_design.input.PX_METADATA_JSON = ".px_metadata.json"
 exp_design.input.NGS_METADATA_JSON = ".ngs_metadata.json"
+exp_design.input.TEST_METADATA_JSON = ".test_metadata.json"
 
 exp_design.output = types.SimpleNamespace()
 exp_design.output.ANY = "exp_design.output.ANY"
 exp_design.output.UTMX_METADATA_CSV = ".utmx_metadata.csv"
 exp_design.output.PX_METADATA_CSV = ".px_metadata.csv"
 exp_design.output.NGS_METADATA_CSV = ".ngs_metadata.csv"
+exp_design.output.TEST_METADATA_JSON = ".test_metadata.json"
 
 
 test = types.SimpleNamespace()
