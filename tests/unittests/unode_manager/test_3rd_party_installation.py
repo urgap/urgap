1052e315e7c5caafbf706a89ff17ff1b9a652539
Tristan Ranff <git@ranff.me>
2023-03-30 09:22:17 +0000

    reinit nodes if required during requirements check

--- tests/unittests/unode_manager/test_3rd_party_installation.py
+++ tests/unittests/unode_manager/test_3rd_party_installation.py
@@ -9,3 +9,8 @@ def test__test_command_not_available():
     is_available = um._test_command(command_list=["ls", "", "-"])
     # having space in the list will always crash....
     assert is_available is False
+
+
+def test_has_all_required_third_party_installs_without_init():
+    assert test_node.has_all_required_installations() is True
+    assert test_node.has_all_required_installations() is True
