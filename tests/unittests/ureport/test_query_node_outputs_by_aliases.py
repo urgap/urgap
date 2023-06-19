dfa75bfdae0078bf982e7bbf455adc33c3062f48
Tristan Ranff <git@ranff.me>
2023-06-19 07:32:04 +0000

    fix node alias discrimination test

--- tests/unittests/ureport/test_query_node_outputs_by_aliases.py
+++ tests/unittests/ureport/test_query_node_outputs_by_aliases.py
@@ -53,6 +53,7 @@ def test_provides_only_specified_uftype(provide_clean_test_node_dirs):
 
     report.draw_execution_dag()
     queried_results = report.query_node_outputs_by_aliases(nodes={0: []})
+    queried_results_with_less = report.query_node_outputs_by_aliases(nodes={2: []})
     queried_results_combined = report.query_node_outputs_by_aliases(
     )
     assert results == queried_results
