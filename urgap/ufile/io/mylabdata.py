5cd2d7d94950756d8bcce9e82f439880433b6bee
Tristan Ranff <git@ranff.me>
2023-06-28 09:36:21 +0000

    first iteration of mylabdata scheme implementation

---
+++ urgap/ufile/io/mylabdata.py
@@ -0,0 +1,35 @@
+import logging
+
+import requests
+
+
+
+
+
+
+
+class IOMyLabData(UIOBase):
+
+
+        Args:
+        """
+
+    @property
+        """Get remote file path.
+
+        Returns:
+        """
+        return None
+
+    @property
+        """Get remote file tag path.
+
+        Returns:
+        """
+        return None
+
+        else:
+
+            response = requests.post(
+                url=url,
+            )
