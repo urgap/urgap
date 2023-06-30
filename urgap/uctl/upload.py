33d8749b21e45e0fb88e68f9c92404af1ff87d9e
Christian Fufezan <christian@fufezan.net>
2023-06-30 09:58:48 +0000

    Add uctl upload feature so md5 and sha1 are set during upload

---
+++ urgap/uctl/upload.py
@@ -0,0 +1,28 @@
+
+from pathlib import Path
+
+import click
+
+
+
+@click.command()
+@click.argument("folder")
+@click.argument("storage_base_uri")
+@click.argument("bucket_structure")
+
+
+
+    """
+
+
+    base_folder = Path(folder)
+    with tqdm(
+        total=len(all_files),
+        desc="Uploading",
+        ncols=60,
+        leave=False,
+        dynamic_ncols=True,
+        miniters=1,
+    ) as pbar:
+        for file in all_files:
+            pbar.update(1)
