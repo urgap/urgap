9d215552bd819d4d468e8e64c33aba29071cc16c
Tristan Ranff <git@ranff.me>
2023-07-04 11:45:07 +0000

    add native google cloud storage implementation

---
+++ urgap/ufile/io/google_storage.py
@@ -0,0 +1,51 @@
+import logging
+
+from google.cloud import storage
+
+
+
+class IOGoogleCloudStorage(UIOBase):
+
+        """Create new UIO class for processing Google Cloud Storage.
+
+        Args:
+        """
+
+    @property
+        """Get remote file path.
+
+        Returns:
+        """
+        return None
+
+    @property
+        """Get remote file tag path.
+
+        Returns:
+        """
+        return None
+
+        """Get remote tags associated with referenced file.
+
+        Returns:
+        """
+        return blob.metadata
+
+        """Upload scratch file to remote location with associated tags.
+
+        Args:
+        """
+            self.blob.metadata = tags
+        self.blob.upload_from_filename(filename=self.scratch_path)
+
+        download_object = False
+        if self.scratch_path.exists():
+            )
+        else:
+            download_object = True
+        if download_object is True:
+
+        Args:
+
+        Returns:
+        """
