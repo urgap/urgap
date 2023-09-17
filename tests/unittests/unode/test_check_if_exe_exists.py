b4ece4d75917bb7d4278940d4ffc99426c5f0337
Valentin Petzold <valentin.g.petzold@gsk.com>
2023-09-17 10:24:44 +0000

    fix temporary check_if_exe_exists test

--- tests/unittests/unode/test_check_if_exe_exists.py
+++ tests/unittests/unode/test_check_if_exe_exists.py
@@ -1,6 +1,9 @@
 
 
 def test_unode_check_if_all_exe_exist():
+    assert output is True
 
 
 def test_unode_check_if_all_exe_exist_wrong_exe():
+    output = unode.check_if_all_exe_exist(["tandem"])
+    assert output is False
