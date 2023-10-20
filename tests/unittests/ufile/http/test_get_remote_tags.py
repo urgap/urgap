75af39f10c6431d96a7b1eeebb91a5b456bc77d9
Valentin Petzold <valentin.g.petzold@gsk.com>
2023-10-20 12:21:50 +0000

    adjust test

--- tests/unittests/ufile/http/test_get_remote_tags.py
+++ tests/unittests/ufile/http/test_get_remote_tags.py
@@ -11,6 +11,8 @@ def test_get_remote_tags_omssa_2_1_9():
 def test_get_remote_tags_test_node_v8():
     node_urn = "linux/x86_64/test_node_v8.zip"
     remote_tag = ufile.io.get_remote_tags()
+    assert remote_tag["md5"] == "1fbb335b02cbc552bc0302bbc7f83a0a"
+    assert "note" in remote_tag.keys()
 
 
 def test_get_remote_tags_no_file():
