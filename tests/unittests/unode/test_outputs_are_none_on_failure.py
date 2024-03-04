befc9d21181638483020b43b35b0e52eaf0bb4d6
Tristan Ranff <git@ranff.me>
2024-03-04 15:27:13 +0000

    add tests

---
+++ tests/unittests/unode/test_outputs_are_none_on_failure.py
@@ -0,0 +1,5 @@
+
+
+def test_node_fail_produces_none():
+        [
+    )
---
+++ urgap/unodes/test_nodes/test_node_8.py
@@ -0,0 +1,42 @@
+
+
+
+    META_INFO = {
+        "wrapper_version": {"major": 1, "minor": 0, "patch": 0},
+        "engine_type": ("test_engine",),
+        "input_uftypes": {
+                "min": 1,
+                "max": 1,
+            },
+        },
+        "output_uftypes": {
+                "min": 1,
+                "max": 1,
+            },
+        },
+    }
+
+
+    def execute(
+        self,
+
+        Args:
+            utrace: Combination of urun_dict, ufile_list and unode.meta.
+
+        Returns:
+            UTrace object, combination of urun_dict, ufile_list and unode.meta.
+        """
+        super().execute(utrace)
+        return utrace
+
+    def preflight(
+        self,
+
+        Args:
+            utrace: Combination of urun_dict, ufile_list and unode.meta.
+
+        Returns:
+            UTrace object, combination of urun_dict, ufile_list and unode.meta.
+        """
+        utrace.urun_dict.command_list = ["python", "-c", "import sys;sys.exit(420)"]
+        return utrace
