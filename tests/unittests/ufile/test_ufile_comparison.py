91f4bceaa9c9c2dc7daaddc5d529d059fd9a54fc
Valentin Petzold <valentin.g.petzold@gsk.com>
2024-03-06 14:14:02 +0000

    black

--- tests/unittests/ufile/test_ufile_comparison.py
+++ tests/unittests/ufile/test_ufile_comparison.py
@@ -2,6 +2,7 @@
 
 def test_same_file_same_location(tmp_file):
     content = "Why so serious?"
+    with open(tmp_file, "w") as file:
         file.write(content)
 
     assert ufile1 == ufile2
@@ -10,6 +11,7 @@ def test_same_file_same_location(tmp_file):
 def test_same_file_different_location(tmp_file):
     content = "Why so serious?"
     for path in [tmp_file, second_file]:
+        with open(path, "w") as file:
             file.write(content)
 
     assert ufile1 != ufile2
