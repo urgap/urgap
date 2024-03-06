4ce8a31b299c707b224bed512bf92e43d3219dce
Valentin Petzold <valentin.g.petzold@gsk.com>
2024-03-06 14:46:26 +0000

    fix test

--- tests/unittests/ufile/test_ufile_comparison.py
+++ tests/unittests/ufile/test_ufile_comparison.py
@@ -1,3 +1,6 @@
+import tempfile
+from pathlib import Path
+
 
 
 def test_same_file_same_location(tmp_file):
@@ -10,6 +13,8 @@ def test_same_file_same_location(tmp_file):
 
 def test_same_file_different_location(tmp_file):
     content = "Why so serious?"
+    with tempfile.NamedTemporaryFile() as tmp_file_2:
+        second_file = Path(tmp_file_2.name)
     for path in [tmp_file, second_file]:
         with open(path, "w") as file:
             file.write(content)
