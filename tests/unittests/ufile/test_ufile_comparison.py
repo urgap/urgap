509e32333d1bb820bbda67373b89197e1be3da04
Valentin Petzold <valentin.g.petzold@gsk.com>
2024-03-06 12:33:23 +0000

    new test for ufile comparison

--- tests/unittests/ufile/test_ufile_comparison.py
+++ tests/unittests/ufile/test_ufile_comparison.py
@@ -1,12 +1,14 @@
 
 
 def test_same_file_same_location(tmp_file):
+    content = "Why so serious?"
         file.write(content)
 
     assert ufile1 == ufile2
 
 
 def test_same_file_different_location(tmp_file):
+    content = "Why so serious?"
     for path in [tmp_file, second_file]:
             file.write(content)
 
