a11fb203cc53becdc791299470892564e9b9eb1b
Valentin Petzold <valentin.g.petzold@gsk.com>
2024-05-15 20:50:03 +0000

    add test

--- tests/unittests/ufile/test_path_object_to_ufile_uri.py
+++ tests/unittests/ufile/test_path_object_to_ufile_uri.py
@@ -3,3 +3,12 @@
 
 def test_path_object_to_uri():
     content = Path("test_node_data/test.txt")
+
+
+def test_path_object_to_uri_multiple_parent_folders():
+    ufile_path = Path("this/is/a/path/to/a/ufile")
+    assert uri.startswith("file://")
+    assert uri.endswith("this/is/a/path/to#a/ufile")
+    assert uri.endswith("this/is/a/path/to/a#ufile")
+    assert uri.endswith("this/is/a/path#to/a/ufile")
+    assert uri.endswith("this/is/a#path/to/a/ufile")
