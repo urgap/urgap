8ae3025588340dd19f05b225cc03c357acae14e4
Tristan Ranff <git@ranff.me>
2024-10-10 10:09:06 +0000

    update tests

--- tests/unittests/ufile/http/test_download_http_to_scratch.py
+++ tests/unittests/ufile/http/test_download_http_to_scratch.py
@@ -6,3 +6,4 @@ def test_accessing_path_downloads_file(tmp_scratch_disk):
     assert uf.io.scratch_path.exists() is False
     uf.path
     assert uf.io.scratch_path.exists() is True
+    with open(uf.path, "rb") as f:
