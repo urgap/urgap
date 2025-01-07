d087a9b4ecb970fdfd14a66198582aaef285952b
Tristan Ranff <git@ranff.me>
2025-01-07 10:31:44 +0000

    hotfix for ufile io manager imports

--- urgap/ufile/ufile_io_manager.py
+++ urgap/ufile/ufile_io_manager.py
@@ -7,4 +7,15 @@ class UFileIOManager:
     """
 
 
+        io_modules_schema_map = {
+            "_base": "_base",
+            "azure_blob": "azure",
+            "file": "file",
+            "ftp": "ftp",
+            "google_storage": "gcs",
+            "https": "https",
+            "mylabdata": "mylabdata",
+            "omiq": "omiq",
+        }
+        for io_module, schema in io_modules_schema_map.items():
                 )
