6bd601269f288f27b13a20cc822229692b8b245e
Christian Fufezan <christian@fufezan.net>
2025-01-08 10:19:12 +0000

    Add import_engine_as_python_function back and adjusted META_INFO test to skip new UNodes for now

--- tests/unittests/wrappers/test_wrappers_have_engines_set.py
+++ tests/unittests/wrappers/test_wrappers_have_engines_set.py
@@ -5,3 +5,15 @@ import pytest
 @pytest.mark.parametrize(
 )
 def test_all_engines_present(node_name):
+        if node.META_INFO["platform_independent"]:
+            assert any(
+                [
+                    engine in node.META_INFO["engine"]
+                    for engine in ("platform_independent", "system")
+            )
+        else:
+            assert any(
+                [
+                    engine in node.META_INFO["engine"]
+                    for engine in ("darwin", "linux", "win32", "system")
+            )
