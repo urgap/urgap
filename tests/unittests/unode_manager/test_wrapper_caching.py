796a92f5c2b2e16c2b2f5844b0dc9fefe13a6780
Valentin Petzold <valentin.g.petzold@gsk.com>
2025-01-14 14:24:45 +0000

    fix test

--- tests/unittests/unode_manager/test_wrapper_caching.py
+++ tests/unittests/unode_manager/test_wrapper_caching.py
@@ -8,6 +8,7 @@ def test_uimporter_import_wrappers():
 
 def test_uimporter_import_unodes():
     """Test if new u3 structure of imports work using example BasicFunctionTestNode."""
+    for expected_version in ["1.1.0", "1.3.0", "latest"]:
         assert (
             )
             is not None
