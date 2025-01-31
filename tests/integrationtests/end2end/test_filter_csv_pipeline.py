2024c76bc2186468ef3aa513ae0de9dca21b6181
Valentin Petzold <valentin.g.petzold@gsk.com>
2025-01-31 16:09:00 +0000

    add pipeline test for filter csv

---
+++ tests/integrationtests/end2end/test_filter_csv_pipeline.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+
+def test_filter_csv_pipeline(tmp_dir):
+        [
+    )
+        {
+            "parameters": {
+            },
+            "unode_parameters": {
+                "storage_base_uri": f"file://{tmp_dir}",
+            },
+    )
+        {
+            "unode_parameters": {
+                "storage_base_uri": f"file://{tmp_dir}",
+                "latest_exe_paths": {
+                    / "resources"
+                    / "Compressor"
+                    / "1_0_0"
+                },
+            },
+    )
+    assert df["sequence_start"].sum() == 9925
+    assert df.shape[0] == 31
+
+
+    )
+    )
+    assert df["sequence_start"].sum() == 1144
+    assert df.shape[0] == 4
