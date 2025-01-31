6c77d7e22d53d703e570bbc675d1a4b193797add
Valentin Petzold <valentin.g.petzold@gsk.com>
2025-01-31 14:36:35 +0000

    add new compress zip wrapper

---
+++ tests/integrationtests/wrappers/test_compress_to_zip.py
@@ -0,0 +1,37 @@
+
+
+def test_wrapper_compress_to_tar(tmp_dir):
+    )
+        {
+            "parameters": {"CompressToZip:1.0.0": {}},
+            "unode_parameters": {
+                "storage_base_uri": f"file://{tmp_dir}",
+            },
+    )
+    result_tar = CompressToZip_node.run(urun_dict=urun_dict, ufiles=ufiles)
+    assert result_tar[0].path.exists()
+    assert result_tar[0].path.suffix == ".zip"
+    result_ufl = result_tar[0].uncompress()
+    assert result_ufl.all_remote_files_exist
+    assert len(ufiles) == len(result_ufl) - 1
+
+
+def test_wrapper_compress_to_tar_latest(tmp_dir):
+    )
+        {
+            "parameters": {"CompressToZip:latest": {}},
+            "unode_parameters": {
+                "storage_base_uri": f"file://{tmp_dir}",
+                "latest_exe_paths": {
+                    / "resources"
+                    / "Compressor"
+                    / "1_0_0"
+                },
+            },
+    )
+    result_tar = CompressToZip_node.run(urun_dict=urun_dict, ufiles=ufiles)
+    assert result_tar[0].path.exists()
+    assert result_tar[0].path.suffix == ".zip"
+    result_ufl = result_tar[0].uncompress()
+    assert result_ufl.all_remote_files_exist
+    assert len(ufiles) == len(result_ufl) - 1
---
+++ urgap/unodes/compress_to_zip/compress_to_zip.py
@@ -0,0 +1,48 @@
+
+import json
+
+
+
+
+    This class allows to tar compress a UFileList of UFiles and Tags.
+    """
+
+    META_INFO = {
+        "name": "CompressToZip",
+        "wrapper_version": {"major": 1, "minor": 0, "patch": 0},
+        "versions": [
+            {"version": "1.0.0", "exe_path": "Compressor/1_0_0/compressor.py"},
+        ],
+        "parameters_not_triggering_rerun": [],
+        "engine": None,
+        "engine_type": ("io",),
+    }
+
+        """Initialize CompressToZip class."""
+
+    def preflight(
+        self,
+        """Preflight routine for CompressToZip wrapper.
+
+        Args:
+            utrace: Combination of urun_dict, ufile_list and unode.meta.
+
+        Returns:
+            UTrace object, combination of urun_dict, ufile_list and unode.meta.
+        """
+        input_files = []
+        for file in utrace.input_files:
+        output_file = utrace.output_files.get_path_objects_by_uftype(
+        )[0]
+
+        utrace.urun_dict.command_list = ["python", str(self.exe_path)]
+        for file in input_files:
+            utrace.urun_dict.command_list.extend(["-i", str(file)])
+        utrace.urun_dict.command_list.extend(
+            [
+                "-o",
+                str(output_file),
+                "-cf",
+                "zip",
+        )
+        return utrace
