0f37033f425060ddc5972c5eb33a61e29b6a53d8
Valentin Petzold <valentin.g.petzold@gsk.com>
2025-02-04 13:11:15 +0000

    add sonarqube coverage to u3 resources

--- urgap/resources/FilterTabular/1_0_0/filter_tabular.py
+++ urgap/resources/FilterTabular/1_0_0/filter_tabular.py
@@ -8,14 +8,67 @@ from pathlib import Path
 import pandas as pd
 
 
+def sense_file_format(file: str) -> str:
+    """Sense the format of a file.
+
+    Args:
+        file: File to analyze.
+
+    Returns:
+        File format as string.
+    """
+        match f.read(4):
+            case b"PAR1":
+                f.seek(-4, 2)
+                if f.read(4) == b"PAR1":
+                    return "parquet"
+            case b"PK\x03\x04":
+                with zipfile.ZipFile(file, "r") as zip_file:
+                    required_files = ["[Content_Types].xml", "xl/"]
+                    if any(file in zip_file.namelist() for file in required_files):
+                        return "xlsx"
+        return "csv"
+
+
+    """Get the separate input file lists for csv, parquet and xlsx.
+
+    Args:
+        input_files: All input files.
+
+    Returns:
+        Input files split up in corresponding lists.
+    """
+    for file in input_files:
+        input_file_type = sense_file_format(file)
+
+
+
+    Args:
+
+    Returns:
+    """
+
+
 
     Args:
 
     """
 
 
-    for file in input_files:
-        input_file_type = sense_file_format(file)
+def main(
+
+    Args:
+        sep: Seperator or delimiter. Defaults to ','
+        mode: Defines the output file type.
+        input_files: Path to files (can be multiple).
+
+        query_string (str): pandas df.query style strings.
+            Note: It is worth selecting each column by encapsulating it with
+            `\ in order to avoid errors, e.g.:
+                " -1 < \`Accuracy (ppm)\` < 1 and \`MS-GF:RawScore\` > 10"
+
+        output (str): Path to output file post filtering.
+    """
     else:
 
 
@@ -37,10 +90,13 @@ if __name__ == "__main__":
     )
     parser.add_argument(
     )
+    parser.add_argument(
+    )
     args = parser.parse_args()
     main(
         input_files=args.input_files,
         output=args.output_file,
         query_string=args.query_string,
         mode=args.mode,
+        sep=args.sep,
     )
