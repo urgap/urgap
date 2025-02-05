480fef64567b1cf1bfa23b8771289e921e645b90
Valentin Petzold <valentin.g.petzold@gsk.com>
2025-02-05 10:45:10 +0000

    add typehints and docstrings

--- urgap/utelemetry.py
+++ urgap/utelemetry.py
@@ -1,3 +1,4 @@
+
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
     OTLPSpanExporter,
 )
@@ -13,6 +14,7 @@ class UTelemetry:
         self.span_lookup = {}
         self.counters = {}
 
+    def init_meter(self) -> metrics.Meter:
         if UTelemetry.metric_was_initialized is False:
             metrics.set_meter_provider(
                 MeterProvider(
@@ -22,6 +24,7 @@ class UTelemetry:
             )
             UTelemetry.metric_was_initialized = True
 
+    def init_tracer(self) -> trace.Tracer:
         if UTelemetry.trace_was_initialized is False:
             trace.set_tracer_provider(
                 TracerProvider(
@@ -31,6 +34,18 @@ class UTelemetry:
             )
                 )
 
+
+        """
+
+    def init_span(
+        self,
+    ) -> trace.Span:
+        """Start and return a new OpenTelemetry span.
+
+        Args:
+
+        Returns:
+        """
         current_tree = self.trace_tree
         parent_context = None
 
@@ -52,16 +67,44 @@ class UTelemetry:
             final_span.add_event(event)
         return final_span
 
+        self,
+        nested_span_list: list,
+        event: str,
+
+        Args:
+        """
         if span is None:
         else:
             span.add_event(event)
 
+        self,
+        nested_span_list: list,
+        events: list,
+
+        Args:
+        """
+        for event in events:
+            if span is None:
+            else:
                 span.add_event(event)
 
+        self,
+        nested_span_list: list,
+        key: str,
+        value: str,
+
+        Args:
+        """
         if span is None:
         else:
             span.set_attribute(key, value)
 
+        self,
+        nested_span_list: list,
+        attributes: dict,
+
+        Args:
+        """
         if span is None:
         else:
             span.set_attributes(attributes)
@@ -69,10 +112,14 @@ class UTelemetry:
         container = self._find_container(nested_span_list)
         if container is None:
 
+    def _find_container(self, nested_span_list: list) -> dict:
         container = None
             container = self.span_lookup[lookup_key]
         return container
 
+
+        Args:
+        """
         container = self._find_container(nested_span_list)
         if container is not None:
             self._close_node_recursive(container)
