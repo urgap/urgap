e9a8c358b2727cd01795ded8fbb585ccfd8a62df
Valentin Petzold <valentin.g.petzold@gsk.com>
2025-02-12 15:52:12 +0000

    add utelemetry test and rename utelemetry method to add_span_attribute

---
+++ tests/unittests/utelemetry/test_utelemetry.py
@@ -0,0 +1,18 @@
+import subprocess
+
+
+def test_utelemetry_run(provide_changeable_config):
+        [
+    )
+        {
+            "parameters": {
+                "FilterTabularToCSV:1.0.0": {
+                    "-q": "`spectrum_id` > 3000",
+            },
+            "unode_parameters": {
+            },
+    )
+
+
+def test_utelemetry_generates_output():
+    assert '"name": "ufiles-uploaded"' in result.stdout
