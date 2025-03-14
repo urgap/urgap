5111c2716eab129f708e4ab5262e06f9a425f49f
Bhupindersingh Khural <bhupindersingh.i.khural@gsk.com>
2025-03-14 08:21:16 +0000

    feat: azure datalake IO

---
+++ tests/unittests/ufile/azure/test_azure_datalake_access_native.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+
+def test_azure_datalake_native_io():
+    try:
+    except KeyError:
+        pytest.skip("Azure backend not available")
+    content = Path("test_node_data/test.txt")
+
+    uf.rebase(
+        "?tenant-id=63982aff-fb6c-4c22-973b-70e4acfb63e6&client-id=f531f934-437c-4a5a-aac2-cd4099125380&temp=hot",
+        upload=True,
+    )
+
+    object_name = uf.object_name
+    uf.purge_local()
+
+    )
+
+    assert new_uf.path.read_text() in uf.path.read_text()
+    new_uf.purge_local()
+    assert new_uf.io.scratch_path.exists() is False
+    new_uf.path
+    assert new_uf.io.scratch_path.exists() is True
+    assert new_uf.tags.get("temp", None) == "hot"
---
+++ urgap/ufile/io/azure_datalake.py
@@ -0,0 +1,114 @@
+
+import logging
+import re
+
+from azure.core.exceptions import (
+    AzureError,
+    ClientAuthenticationError,
+    HttpResponseError,
+    ResourceExistsError,
+)
+
+
+
+class IOAzureDL(UIOBase):
+
+        Args:
+        """
+        self.client_keys = ["tenant-id", "client-id"]
+        for key_name in self.client_keys:
+        self.datalake_service_client = DataLakeServiceClient(
+            credential=ClientSecretCredential(
+            ),
+        )
+
+        available_file_systems = [
+            x["name"] for x in self.datalake_service_client.list_file_systems()
+        ]
+            )
+
+        self.file_system_client = self.datalake_service_client.get_file_system_client(
+        )
+        self.directory_client = self.file_system_client.get_directory_client(
+        )
+        self.object_directory_client = self.file_system_client.get_directory_client(
+        )
+        logging.getLogger("azure.core.pipeline.policies.http_logging_policy").setLevel(
+        )
+
+        """Close datalake connection on object deletion."""
+
+    @property
+        Returns:
+        """
+        return None
+
+        Returns:
+        """
+        if self.remote_object_exists() is True:
+            return self.file_client.get_file_properties()
+
+        Returns:
+        """
+        if self.remote_object_exists() is True:
+            return self.get_file_properties()["metadata"]
+
+        Returns:
+        """
+        if self.remote_object_exists() is True:
+            return self.get_file_properties()["name"]
+
+        """
+        try:
+            download = self.file_client.download_file()
+                download.readinto(ooo)
+        except (
+            AzureError,
+            ServiceRequestError,
+            ClientAuthenticationError,
+            ResourceNotFoundError,
+            HttpResponseError,
+            self.scratch_path.unlink(missing_ok=True)
+
+        file_dir_list = self.file_client.path_name.split("/")[:-1]
+        for n in range(len(file_dir_list)):
+            tmp_dir_client = self.file_system_client.get_directory_client(
+            )
+                tmp_dir_client.create_directory()
+        try:
+            self.file_client.create_file()
+            for keyname in self.client_keys:
+
+                self.file_client.upload_data(data, metadata=tags, overwrite=True)
+        except (
+            AzureError,
+            ServiceRequestError,
+            ClientAuthenticationError,
+            ResourceNotFoundError,
+            HttpResponseError,
+
+        if tags is not None:
+            self.file_client.set_metadata(tags)
+
+    def remote_object_exists(self) -> bool:
+        Returns:
+        """
+        return self.file_client.exists()
+
+    def _remote_path_exists(self) -> bool:
+        Returns:
+        """
+        try:
+            self.directory_client.get_directory_properties()
+        except ResourceNotFoundError:
+            return False
+
+        Args:
+        Returns:
+        """
+        container_objects = self.file_system_client.get_paths(recursive=False)
+        if pattern is not None:
+            container_objects = [
+                f for f in container_objects if re.search(pattern, f) is not None
+            ]
+        return container_objects
