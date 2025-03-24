0002e49fc6556cb3d63960a02ff861c20932b9a1
Valentin Petzold <valentin.g.petzold@gsk.com>
2025-03-24 12:21:26 +0000

    add remote e2e test

---
+++ tests/integrationtests/end2end/test_filter_csv_pipeline_remote.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+
+
+        [
+    )
+        {
+            "parameters": {
+                "FilterTabularToCSV:latest": {
+                    "-q": "500 < `exp_mz` < 1000",
+            },
+            "unode_parameters": {
+                "storage_base_uri": f"file://{tmp_dir}",
+                "remote_url": "http://localhost",
+                "latest_exe_paths": {
+                    / "resources"
+                    / "FilterTabular"
+                    / "1_0_0"
+                },
+            },
+    )
+        {
+            "parameters": {"CompressToTar:latest": {}},
+            "unode_parameters": {
+                "remote_url": "http://localhost",
+                "storage_base_uri": f"file://{tmp_dir}",
+                "latest_exe_paths": {
+                    / "resources"
+                    / "Compressor"
+                    / "1_0_0"
+                },
+            },
+            "wid": urun_dict_filter["wid"],
+    )
+
+    df = pd.read_csv(filter_1[0].path)
+    assert df["sequence_start"].sum() == 9925
+    assert df.shape[0] == 31
+
+    assert tar_1[0].path.suffix == ".tar"
+    untar_1 = tar_1[0].uncompress()
+
+    urun_dict_filter.parameters["FilterTabularToCSV:latest"].update(
+    )
+    df = pd.read_csv(filtered_1a[0].path)
+    assert df["sequence_start"].sum() == 1144
+    assert df.shape[0] == 4
