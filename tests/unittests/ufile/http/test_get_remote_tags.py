5000da6d3caee99c712318a27ff3a68fce7cc720
Valentin Petzold <valentin.g.petzold@gsk.com>
2025-04-29 13:01:53 +0000

    use uri for tests

--- tests/unittests/ufile/http/test_get_remote_tags.py
+++ tests/unittests/ufile/http/test_get_remote_tags.py
@@ -22,6 +22,7 @@ def test_get_remote_tags_no_file():
 
 
 def test_get_remote_tags_no_tags_but_response():
+        uri="https://github.com/gsk-tech/pymx/raw/main/example_scripts"
     )
     remote_tag = ufile.io.get_remote_tags()
     assert remote_tag is None
