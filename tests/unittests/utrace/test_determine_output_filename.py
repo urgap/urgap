7d1ea93eeed914a83d9c090811ed3c7f8d200632
Tristan Ranff <git@ranff.me>
2025-05-26 08:41:34 +0000

    fix tests for updated default string

--- tests/unittests/utrace/test_determine_output_filename.py
+++ tests/unittests/utrace/test_determine_output_filename.py
@@ -13,6 +13,7 @@ def test_single_input():
     assert (
         bool(
             re.search(
+                r"TestNode4_1.0.0_wx_[0-9a-z]{32}/b8dd6ef5f15f638b208bc7c28df13a19",
                 _output,
         )
         is True
@@ -34,6 +35,7 @@ def test_single_input_no_data_versioning():
     _output = ut.determine_output_files_stem()
 
     assert (
+        bool(re.search(r"TestNode4_1.0.0_wx/b8dd6ef5f15f638b208bc7c28df13a19", _output))
         is True
     )
 
@@ -55,6 +57,7 @@ def test_single_input_with_run_folder_name():
     assert (
         bool(
             re.search(
+                r"MoRunFolder_[0-9a-z]{32}/b8dd6ef5f15f638b208bc7c28df13a19",
                 _output,
         )
         is True
@@ -71,6 +74,7 @@ def test_single_input_with_prefix_and_nested_dir():
     assert (
         bool(
             re.search(
+                "TestNode4_1.0.0_wx_[0-9a-z]{32}/_!_b8dd6ef5f15f638b208bc7c28df13a19",
                 str(_output),
         )
         is True
@@ -107,6 +111,7 @@ def test_multi_input():
     assert (
         bool(
             re.search(
+                r"TestNode4_1.0.0_wx_[0-9a-z]{32}/9dbda1cf7a25c9e8da2b4e7d60be1387",
                 str(_output),
         )
         is True
@@ -127,6 +132,7 @@ def test_multi_input_shuffled_input():
     assert (
         bool(
             re.search(
+                r"TestNode4_1.0.0_wx_[0-9a-z]{32}/9dbda1cf7a25c9e8da2b4e7d60be1387",
                 str(_output),
         )
         is True
@@ -146,6 +152,7 @@ def test_multi_input_with_data_versioning():
     assert (
         bool(
             re.search(
+                r"TestNode4_1.0.0_wx_[0-9a-z]{32}/9dbda1cf7a25c9e8da2b4e7d60be1387",
                 str(_output),
         )
         is True
@@ -170,3 +177,4 @@ def test_override_folder_creation_with_md5():
         input_files=input_files,
     )
     _output = ut.determine_output_files_stem()
+    assert _output == "9dbda1cf7a25c9e8da2b4e7d60be1387"
