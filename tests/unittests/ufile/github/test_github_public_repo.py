c17850a16fad583ad4bf7f788cd02f70c41b2304
Bhupindersingh Khural <bhupindersingh.i.khural@gsk.com>
2025-06-12 15:55:06 +0000

    fix: mock upload

--- tests/unittests/ufile/github/test_github_public_repo.py
+++ tests/unittests/ufile/github/test_github_public_repo.py
@@ -1,5 +1,42 @@
+import pytest
 
     )
 
     )
 
+
+    uf_io_object.remote_object_exists = MagicMock(return_value=True)
+    mock_file_sha = "existing_sha"
+    uf_io_object.repo.get_contents.return_value.sha = mock_file_sha
+    uf_io_object.repo.create_pull.return_value.number = 42
+
+        uf_io_object.upload()
+
+    # Assert
+    uf_io_object.repo.create_git_ref.assert_called_once_with(
+    )
+    uf_io_object.repo.update_file.assert_called_once_with(
+        path="some/path/to/file.txt",
+        message="New ufile is available",
+        content=mock_content,
+        sha=mock_file_sha,
+        branch="fake_target_branch",
+    )
+    uf_io_object.repo.create_pull.assert_called_once()
+
+
+    uf_io_object.remote_object_exists = MagicMock(return_value=False)
+    uf_io_object.repo.create_pull.return_value.number = 42
+
+        # Act
+        uf_io_object.upload()
+
+    # Assert
+    uf_io_object.repo.create_git_ref.assert_called_once()
+    uf_io_object.repo.create_file.assert_called_once_with(
+        path="some/path/to/file.txt",
+        message="New ufile is available",
+        content=mock_content,
+        branch="fake_target_branch",
+    )
+    uf_io_object.repo.create_pull.assert_called_once()
