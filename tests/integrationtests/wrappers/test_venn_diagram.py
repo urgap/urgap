3def0bb356b05483fae35e46310c86ff6a1ce739
Christian Fufezan <christian@fufezan.net>
2025-06-17 07:31:14 +0000

    Add assert content

--- tests/integrationtests/wrappers/test_venn_diagram.py
+++ tests/integrationtests/wrappers/test_venn_diagram.py
@@ -1,5 +1,7 @@
 """Integration test for VennDiagram."""
 
+import re
+
 
 
         [
@@ -14,5 +16,15 @@
                 "storage_base_uri": f"file://{tmp_dir}",
             },
     )
+    expected_patterns_svg = [
+        r"<text[^>]*?>.*?n = 39.*?</text>",
+        r"<text[^>]*?>.*?n = 16.*?</text>",
+        r"<text[^>]*?>.*?n = 28.*?</text>",
+        r"<text[^>]*?>.*?n = 17.*?</text>",
+    ]
     venn_svg = venn_node.run(urun_dict=urun_dict, ufiles=ufiles)
     assert venn_svg[0].path.exists()
+    with open(venn_svg[0].path) as svg_file:
+        svg_content = svg_file.read()
+    assert all(
+    )
