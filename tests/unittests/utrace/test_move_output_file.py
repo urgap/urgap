cd637f6a0b8b97a709b8ff99f6d43a5d8f7074ec
Valentin Petzold <valentin.g.petzold@gsk.com>
2025-06-17 14:40:25 +0000

    cover edge case

--- tests/unittests/utrace/test_move_output_file.py
+++ tests/unittests/utrace/test_move_output_file.py
@@ -31,5 +31,16 @@ import tempfile
         urun_dict=urd,
         input_files=input_files,
     )
+    assert len(ut.output_files) == 2
+    assert ut.output_files[0].path.exists()
+    assert ut.output_files[1].path.exists()
+
+
+        [
+            ),
+    )
+        urun_dict=urd,
+        input_files=input_files,
+    )
     assert ut.output_files[1].path.exists()
     assert len(ut.output_files) == 2
