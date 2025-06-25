408bea45c49cdc2a75affa9b5415a48add8f6897
Bhupindersingh Khural <bhupindersingh.i.khural@gsk.com>
2025-06-25 11:42:35 +0000

    fix: mock all tests

--- tests/unittests/ufile/github/test_github_public_repo.py
+++ tests/unittests/ufile/github/test_github_public_repo.py
@@ -5,9 +5,27 @@ import pytest
 from github import GithubException
 
 
+    # Test the absence of file
+    uf_io_object.repo.get_contents = MagicMock(
+        side_effect=GithubException("Something went wrong"),
     )
+    assert uf_io_object.remote_object_exists() is False
 
+    # Test the file that exist
+    uf_io_object.repo.get_contents = MagicMock(return_value=True)
+    assert uf_io_object.remote_object_exists() is True
+
+    # Test downloading the file
+    uf_io_object.repo.get_contents.return_value = MagicMock(
     )
+    m = mock_open()
+    with patch("pathlib.Path.open", m):
+        uf_io_object.download()
+
+    # Assert file write
+    m.assert_called_once_with("w", encoding="utf-8")
+    handle = m()
+    handle.write.assert_called_once_with("hello from github")
 
 
     uf_io_object.remote_object_exists = MagicMock(return_value=True)
