06f00459f4ecd87e20a3875a0a8797bc404918f5
Valentin Petzold <valentin.g.petzold@gsk.com>
2025-06-27 13:49:14 +0000

    fix move files tests

--- tests/unittests/utrace/test_move_output_file.py
+++ tests/unittests/utrace/test_move_output_file.py
@@ -22,6 +22,8 @@ def test_move_output_file_keep_original_name(tmp_scratch_disk):
         input_files=input_files,
     )
     with tempfile.NamedTemporaryFile(mode="w+", delete=False) as tmp_file:
+        ut.move_output_files(
+            files=[tmp_file.name],
             keep_original_name=True,
         )
     assert ut.output_files[0].path.exists()
@@ -36,6 +38,10 @@ def test_move_output_file_twice(tmp_scratch_disk):
         urun_dict=urd,
         input_files=input_files,
     )
+    ut.move_output_files(
+        files=files,
+        extend_len=1,
+    )
     assert len(ut.output_files) == 2
     assert ut.output_files[0].path.exists()
     assert ut.output_files[1].path.exists()
@@ -48,6 +54,13 @@ def test_move_output_file_twice_with_original_name(tmp_scratch_disk):
         urun_dict=urd,
         input_files=input_files,
     )
+    ut.move_output_files(
+        files=files,
+        extend_len=1,
+        keep_original_name=True,
+    )
     assert ut.output_files[0].path.exists()
     assert ut.output_files[1].path.exists()
     assert len(ut.output_files) == 2
+    assert ut.output_files[0].tags["original_name"] == files[0]
+    assert ut.output_files[1].tags["original_name"] == files[1]
