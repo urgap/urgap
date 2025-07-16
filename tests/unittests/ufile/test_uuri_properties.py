049868706c1efa177cae80d3129eed827c6b1e11
Valentin Petzold <valentin.g.petzold@gsk.com>
2025-07-16 13:06:32 +0000

    add more test coverage

--- tests/unittests/ufile/test_uuri_properties.py
+++ tests/unittests/ufile/test_uuri_properties.py
@@ -39,3 +39,39 @@ def test_uuri_properties_file_schema():
 
 
 def test_uuri_properties_mld():
+    uf = urgap.UFile(uri="mylabdata://dummy#test")
+    assert uf.uuri.mylabdata_api_url == "https://dummy"
+    assert uf.uuri.mylabdata_api_url_files == "https://dummy/files"
+
+
+def test_uuri_properties_samba():
+    uf = urgap.UFile(uri="smb://dummy.test/share/folder#test_node_data/test.txt")
+    assert uf.uuri.samba_share == "share/folder"
+    assert uf.uuri.file_remote_path is None
+    assert uf.uuri.file_remote_tag_path is None
+
+
+def test_uuri_properties_azure_samba():
+    uf = urgap.UFile(uri="az-smb://dummy.test/share/folder#test_node_data/test.txt")
+    assert uf.uuri.azure_share == "share"
+    assert uf.uuri.azure_directory_list == ["folder"]
+    assert uf.uuri.azure_object_file == "test.txt"
+    assert uf.uuri.azure_object_directory_list == ["test_node_data"]
+
+    uf2 = urgap.UFile(uri="az-smb://dummy.test/share/folder#test.txt")
+    assert uf2.uuri.azure_object_directory_list == []
+
+
+def test_uuri_properties_https():
+    ufile = urgap.UFile(
+        uri="https://github.com/gsk-tech/pymx/raw/main/example_scripts"
+        "#filter_nonbiological_ions_1_0_0.py",
+    )
+    assert (
+        ufile.uuri.https_remote_path
+        == "https://github.com/gsk-tech/pymx/raw/main/example_scripts/filter_nonbiological_ions_1_0_0.py"
+    )
+    assert (
+        ufile.uuri.https_remote_tag_path
+        == "https://github.com/gsk-tech/pymx/raw/main/example_scripts/filter_nonbiological_ions_1_0_0.py.tag"
+    )
