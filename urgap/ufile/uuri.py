74ec96b4b9cd2cce4b2a3e8631435b9fa821e559
Valentin Petzold <valentin.g.petzold@gsk.com>
2025-12-11 11:45:26 +0100

    add gh action to run tests

--- urgap/ufile/uuri.py
+++ urgap/ufile/uuri.py
@@ -38,7 +38,7 @@ class UUri:
         self.uri_dict = urlparse(uri)._asdict()
         self.scheme = self.uri_dict["scheme"]
         self.netloc = self.uri_dict["netloc"]
-        self.path = self.uri_dict["path"]
+        self.path = urllib.request.url2pathname(self.uri_dict["path"])
         self.params = self.uri_dict["params"]
         self.original_query = self.uri_dict["query"]
         self.fragment = self.uri_dict["fragment"]
